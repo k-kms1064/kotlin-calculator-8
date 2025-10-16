fc25507668851035f2383e7142418cd4981ec9e7
k-kms1064 <kms106418@gmail.com>
2025-10-16 17:51:48 +0900

    feat(calculator): 입력한 문자열의 숫자를 더하는 계산 기능 구현

---
+++ src/main/kotlin/calculator/StringAddCalculator.kt
@@ -0,0 +1,14 @@
+package calculator
+
+class StringAddCalculator {
+
+    fun calculate(input: String?): Int {
+        if (input.isNullOrBlank()) {
+            return 0
+        }
+
+        val numbers = input.split(",")
+
+        return numbers.sumOf { it.toInt() }
+    }
+}
